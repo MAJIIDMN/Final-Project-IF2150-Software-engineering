2bcd5cc6ff065a8210c306ef7762ec69b544b284
Ray Owen <rayowenmartin2018@gmail.com>
2025-11-24 19:02:49 +0700

    feat: skeleton code for home UI

--- src/views/order/general_details.py
+++ src/views/order/general_details.py
@@ -1,11 +1,29 @@
 import flet as ft
 from components.shared import create_header, create_sidebar
 
+from views.order.address import AddressView
+from views.order.navigation import NavigationView
+from views.order.date_and_time import DateAndTimeView
+
 fonts = {
     "Poppins": "fonts/poppins/Poppins-Regular.ttf",
     "PoppinsBold": "fonts/poppins/Poppins-Bold.ttf",
     "PoppinsSBold": "fonts/poppins/Poppins-SemiBold.ttf",
 }
+
+class OrderState:
+    def __init__(self):
+        self.waste_types = []
+        self.weight = ""
+        self.condition = "Good"
+        self.attachment = None
+        self.district = ""
+        self.address = ""
+        self.notify_on_arrival = False
+        self.selected_date = None
+        self.selected_time = None
+        self.selected_collector = None
+        self.completed_steps = set()
 
 def GeneralDetailsView(page, order_state):
     selected_types = order_state.waste_types if order_state.waste_types else []
@@ -328,3 +346,33 @@ def GeneralDetailsView(page, order_state):
         padding=0,
         expand=True,
     )
+
+def main(page: ft.Page):
+    page.title = "GrowBak"
+    page.window_width = 1440
+    page.window_height = 1024
+    page.padding = 0
+    
+    order_state = OrderState()
+    content_area = ft.Container(expand=True)
+    
+    def navigate_to(route):
+        content_area.content = None
+        
+        if route == "/order/general-details":
+            content_area.content = GeneralDetailsView(page, order_state)
+        elif route == "/order/address":
+            content_area.content = AddressView(page, order_state)
+        elif route == "/order/date-and-time":
+            content_area.content = DateAndTimeView(page, order_state)
+        elif route == "/order/navigation":
+            content_area.content = NavigationView(page, order_state)
+        
+        page.update()
+    
+    def route_change(route):
+        navigate_to(page.route)
+    
+    page.on_route_change = route_change
+    page.add(content_area)
+    page.go("/order/general-details")
