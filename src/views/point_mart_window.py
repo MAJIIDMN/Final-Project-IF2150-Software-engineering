867ab152225507c07de21a0a7c21b45e65aa02c5
MAJIIDMN <muhammadnurmajiid22@gmail.com>
2025-11-25 21:52:24 +0700

    fix : sort avail product

--- src/views/point_mart_window.py
+++ src/views/point_mart_window.py
@@ -69,6 +69,12 @@ def main(page: ft.Page):
     products = point_mart.load_hadiah()
 
     def sort_by_point(e, reverse: bool, hadiah: list):
+        if checkbox_availability.current.value and not checkbox_out_of_stock.current.value:
+            hadiah = avail_hadiah
+        elif not checkbox_availability.current.value and checkbox_out_of_stock.current.value:
+            hadiah = non_avail_hadiah
+        elif not checkbox_availability.current.value and not checkbox_out_of_stock.current.value:
+            hadiah = []
         products_grid.controls.clear()
         if reverse == True:
             hadiah = point_mart.sort_by_points_down(hadiah)
@@ -81,6 +87,13 @@ def main(page: ft.Page):
 
     def sort_by_stock(e, reverse: bool, hadiah: list):
         products_grid.controls.clear()
+        if checkbox_availability.current.value and not checkbox_out_of_stock.current.value:
+            hadiah = avail_hadiah
+        elif not checkbox_availability.current.value and checkbox_out_of_stock.current.value:
+            hadiah = non_avail_hadiah
+        elif not checkbox_availability.current.value and not checkbox_out_of_stock.current.value:
+            hadiah = []
+
         if reverse == True:
             hadiah = point_mart.sort_by_stock_down(hadiah)
         else:
